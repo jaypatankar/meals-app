5393665d8ce927ec67fd75c9fafad5fe349dc701
jaypatankar <jaypatankar2510@gmail.com>
2021-06-08 12:45:11 +0530

    pushReplacementNamed and Back to Drawer navigation

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   // const FiltersScreen({ Key? key }) : super(key: key);
@@ -7,8 +8,13 @@ class FiltersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('FilterScreen'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Title"),
+        ),
+        drawer: MainDrawer(),
+        body: Center(
+          child: Text('FilterScreen'),
+        ));
   }
 }
